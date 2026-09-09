9d549cbe52c5409c4e337bc2d61104c751043d86
Vincannes <vincent.trolard@gmail.com>
2026-09-09 09:06:49 +0200

    updaet

--- app/domain/model/order_model.py
+++ app/domain/model/order_model.py
@@ -12,15 +12,34 @@ class OrderModel(object):
     ORDER_TYPE_BUY = 1
     ORDER_TYPE_SELL = 2
 
-    def __init__(self, order_type, pair, price, stop, profits):
+    ACTION_OPEN = "open"
+    ACTION_BREAKEVEN = "breakeven"
+    ACTION_CLOSE = "close"
+
+    def __init__(self, order_type, pair, price, stop, profits, action=ACTION_OPEN):
         self._order_type = order_type
         self._pair = pair
         self._price = price
         self._stop = stop
         self._profits = profits
+        self._action = action
+
+    @property
+    def action(self):
+        return self._action
+
+    @property
+    def is_breakeven(self):
+        return self._action == self.ACTION_BREAKEVEN
+
+    @property
+    def is_close(self):
+        return self._action == self.ACTION_CLOSE
 
     @property
     def pair(self):
+        if not self._pair:
+            return ""
         return self._pair.upper()
 
     @property
@@ -37,10 +56,25 @@ class OrderModel(object):
 
     @property
     def profits(self):
-        return sorted([to_float(i) for i in self._profits])
+        # TP1 is the closest target to the entry price: ascending for a Buy,
+        # descending for a Sell.
+        values = [to_float(i) for i in self._profits]
+        return sorted(values, reverse=self._order_type == self.ORDER_TYPE_SELL)
 
     def to_dict(self):
+        if self.is_close:
+            # No pair -> MQL5 closes every position/order opened by this EA.
+            return {
+                "action": self.ACTION_CLOSE,
+                "symbol": self.pair,
+            }
+        if self.is_breakeven:
+            return {
+                "action": self.ACTION_BREAKEVEN,
+                "symbol": self.pair,
+            }
         return {
+            "action": self.ACTION_OPEN,
             "symbol": self.pair,
             "type": self.order_type,
             "entry": self.price,
@@ -49,6 +83,14 @@ class OrderModel(object):
         }
 
     def __str__(self):
+        if self.is_close:
+            target = self.pair if self.pair else "ALL open positions"
+            return "Close : '{}' (close trades)".format(target)
+
+        if self.is_breakeven:
+            target = self.pair if self.pair else "ALL open positions"
+            return "BreakEven : '{}' (move SL to entry)".format(target)
+
         dir_str = "None"
         if self.order_type == self.ORDER_TYPE_BUY:
             dir_str = "Buy"
